e191393fc95aba7f242f331535b3bce60cc5cb3f
Nick <46008898+NickJoannette@users.noreply.github.com>
2019-09-08 23:46:09 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,19 +8,8 @@ void handleAddition(){};
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    // Declare a layout type, declare relevant widgets which will compose the layout, and assign these widgets to the layout
-    // QFormLayout takse a pointer to a widget as its argument to determine its location
-
     MainWindow main_window;
-    // Declare, size, show, and title your window
-
-  //  main_window.resize(250,95);
-
-
     main_window.show();
-
-
     main_window.setWindowTitle("Polynomial Calculator");
-
     return a.exec();
 }
